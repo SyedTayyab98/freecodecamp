a919039f53e19578fad21b63a335cf2d1be8d5ca
tayyab.ali <tayyab.ali@10pearls.local>
2021-01-05 17:47:55 +0500

    add task to 5.47pm

---
+++ task94.js
@@ -0,0 +1,8 @@
+// Setup
+var i = 5;
+var myArray = [];
+while (i >= 0) {
+    myArray.push(i);
+    i--;
+}
+// Only change code below this line
---
+++ task97.js
@@ -0,0 +1,6 @@
+// Setup
+var myArray = [];
+for (let i = 10; i > 0; i -= 2) {
+    myArray.push(i - 1);
+}
+// Only change code below this line
