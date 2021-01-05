8dc08944442c7f412bba6b03bb8adc870cc34220
tayyab.ali <tayyab.ali@10pearls.local>
2021-01-05 16:50:38 +0500

    add new files to git at 4.50pm 5 jan 2021

---
+++ task81.js
@@ -0,0 +1,8 @@
+var myDog = {
+    // Only change code below this line
+    'name': 'tommy',
+    'legs': 4,
+    'tails': 1,
+    'friends': ['max', 'jackie']
+        // Only change code above this line
+};
---
+++ task90.js
@@ -0,0 +1,18 @@
+var myMusic = [{
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        'artist': 'tayyab',
+        'title': 'rockup',
+        'release_year': 2021,
+        'formats': ['CDs', 'radio'],
+    }
+];
